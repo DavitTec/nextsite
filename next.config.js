c89c75c5bdcb80cac0f0232090ac29f5a2ddf255
DavitTec <davittech@davit.ie>
2022-02-25 23:48:25 +0100

    v0.1.1  Remodel

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-module.exports = {
-  exportPathMap: async function(
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-      "/about": { page: "/about" }
-    };
-  }
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
